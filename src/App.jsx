b8ab10b397ed88609dbd4e86ca6bd59ae42b43cd
Digvijaysinh <digvijaysinhatodariya1@gmail.com>
2025-12-03 15:25:48 +0530

    hero section add kiya hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from "./component/Footer";
+import Hero from "./component/hero";
 import Navbar from "./component/Navbar";
 
 export default function mainPage(){
@@ -6,6 +7,7 @@ export default function mainPage(){
   return(
     <>
     <Navbar />
+    <Hero />
     <Footer />
 
     </>
