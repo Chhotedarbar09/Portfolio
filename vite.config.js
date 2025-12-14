829f03cba558ae79eca99441ea7fe46100ae6aef
Digvijaysinh <digvijaysinhatodariya1@gmail.com>
2025-12-14 23:19:55 +0530

    Initial React portfolio commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // yahin CAPITAL P, repo name jaisa hi
+  base: '/Portfolio/', // yahin CAPITAL P, repo name jaisa hi
 })
